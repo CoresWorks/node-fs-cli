7b608c9e2740575fc1c88ea7b9d984791621c7fd
coresyt <152510458+coresyt@users.noreply.github.com>
2024-07-12 15:12:20 -0600

    docs(./docs): Init Docs v0.5

---
+++ docs/astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: '@fs/cli',
+      social: {
+        github: 'https://github.com/CoresWorks/node-fs-cli',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+});
