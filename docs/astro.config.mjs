4ee168c76adcf5333b253ae629f3ad80603a1cfb
coresyt <152510458+coresyt@users.noreply.github.com>
2024-07-13 16:09:07 -0600

    chore(./docs): Add adapter of vercel

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,24 +1,25 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://node-fs-cli.vercel.app',
   base: '/',
-
-  integrations: [
-    starlight({
-      title: 'fs',
-      favicon: '/node.png',
-      social: {
-        github: 'https://github.com/CoresWorks/node-fs-cli',
-      },
-      sidebar: [
-        {
-          label: 'CLI',
-          autogenerate: { directory: 'cli' },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: 'fs',
+    favicon: '/node.png',
+    social: {
+      github: 'https://github.com/CoresWorks/node-fs-cli'
+    },
+    sidebar: [{
+      label: 'CLI',
+      autogenerate: {
+        directory: 'cli'
+      }
+    }]
+  })],
+  output: "server",
+  adapter: vercel()
 });
