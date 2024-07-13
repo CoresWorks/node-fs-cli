1dddf9385428814ee72e6490f4a0bee137abda48
coresyt <152510458+coresyt@users.noreply.github.com>
2024-07-13 16:02:04 -0600

    chore(./docs): Add vercel config deploy

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,8 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://coresworks.github.io',
-  base: 'node-fs-cli',
+  site: 'https://node-fs-cli.vercel.app',
+  base: '/',
 
   integrations: [
     starlight({
